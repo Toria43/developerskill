a3ce8d5da0f2ef426ca0aed8104c8f17eb416e0b
Victory <ahereosamudiamen@gmail.com>
2024-04-27 09:14:57 +0100

    Splice and Slice explained

--- script.js
+++ script.js
@@ -8,8 +8,12 @@ console.log(course[1]);
 console.log(course[2]);
 console.log(course[3]);
 console.log(course[4]);
-// console.log(course.length - 1);
+// Slice and Splice
+
 console.log(course.slice(2, 4));
 console.log(course);
+/*Slice removes elements into a new array  without changing the original array*/
+
 console.log(course.splice(2, 4));
 console.log(course);
+/*Splice extracts elements into a new array while changing the original array*/
