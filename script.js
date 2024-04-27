0fbf7d16b49ea46383c504a642a54111228b0657
Victory <ahereosamudiamen@gmail.com>
2024-04-27 09:09:56 +0100

    slice and splice

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-"use strict";
+'use strict';
+
+const course = ['Chemistry', 'Botany', 'Zoology', 'Biology', 'French'];
+console.log(course);
+console.log(course.length);
+console.log(course[0]);
+console.log(course[1]);
+console.log(course[2]);
+console.log(course[3]);
+console.log(course[4]);
+// console.log(course.length - 1);
+console.log(course.slice(2, 4));
+console.log(course);
+console.log(course.splice(2, 4));
+console.log(course);
